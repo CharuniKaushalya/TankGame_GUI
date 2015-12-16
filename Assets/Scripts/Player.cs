04eb3189bd2d4caa24e72221f89e9e12fa6974c2
CharuniKaushalya <snkaushi@gmail.com>
2015-12-16 23:47:22 +0530

    build player and player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(GunController))]
+public class Player : LivingEntity
+{
+
+    public float moveSpeed = 5;
+
+    PlayerController controller;
+    GunController gunController;
+
+    protected override void Start()
+    {
+        base.Start();
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+    }
+
+    void Update()
+    {
+        // Movement input
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+
+        // Weapon input
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
+        }
+    }
+}
