eaf41337140724956cadcb48e01b426ee7774958
CharuniKaushalya <CharuniKaushalya@users.noreply.github.com>
2016-03-05 09:31:10 +0530

    Add bricks damage level

---
+++ TileMap/TileMap/TileMap/Dean/Bricks.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TileMap.Dean
+{
+    class Bricks
+    {
+        Point pos;
+        public Bricks(Point position)
+        {
+            pos = position;
+        }
+        int damageLevel = 0;
+
+        public int DamageLevel
+        {
+            get { return damageLevel; }
+            set { damageLevel = value; }
+        }
+    }
+}
