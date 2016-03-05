65ada4d715fb894c4915841394a8ee814328e98c
CharuniKaushalya <CharuniKaushalya@users.noreply.github.com>
2016-03-05 09:30:19 +0530

    Set map items

---
+++ TileMap/TileMap/TileMap/Dean/MapItem.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TileMap.Dean
+{
+    class MapItem
+    {
+         private int locationX;
+        private int locationY;
+        private float lifeTime = 0.0f;
+        private float iTimer = 0.0f;
+        private Point position;
+
+        private float appearTime = 0.0f;
+        private DateTime disappearTime;
+
+        public MapItem(int x, int y)
+        {
+            locationX = x;
+            locationY = y;
+            position = new Point(locationX, locationY);
+
+        }
+        public int LocationX
+        {
+            get;
+            set;
+        }
+        public int LocationY
+        {
+            get;
+            set;
+        }
+        public  float LifeTime
+        {
+            get { return lifeTime; }
+            set { lifeTime = (value);
+            DateTime gametime = DateTime.Now;
+            disappearTime = gametime.AddMilliseconds(lifeTime);
+            }
+        }
+        public float AppearTime
+        {
+            get { return appearTime; }
+        }
+
+        public DateTime DisappearTime
+        {
+            get { return disappearTime; }
+        }
+    }
+}
