8a80ffca4aac86369ac0dcd328c43f85e2823e72
CharuniKaushalya <CharuniKaushalya@users.noreply.github.com>
2016-03-05 23:04:14 +0530

    Changes to algorithm

--- TileMap/TileMap/TileMap/Algorithm/Pathfinder.cs
+++ TileMap/TileMap/TileMap/Algorithm/Pathfinder.cs
@@ -10,24 +10,20 @@ namespace TileMap.Algorithm
     {
         public List<Point> Pathfind(Point start, Point end, int[,] Weight)
         {
-            // nodes that have already been analyzed and have a path from the start to them
-            var closedSet = new List<Point>();
-            // nodes that have been identified as a neighbor of an analyzed node, but have 
-            // yet to be fully analyzed
-            var openSet = new List<Point> { start };
-            // a dictionary identifying the optimal origin point to each node. this is used 
-            // to back-track from the end to find the optimal path
-            var cameFrom = new Dictionary<Point, Point>();
-            // a dictionary indicating how far each analyzed node is from the start
-            var currentDistance = new Dictionary<Point, int>();
-            // a dictionary indicating how far it is expected to reach the end, if the path 
-            // travels through the specified node. 
-            var predictedDistance = new Dictionary<Point, float>();
+            #region Variables
+            var closedSet = new List<Point>();          // cells that already analyzed and have a path from the start to them
+
+            var openSet = new List<Point> { start };    // cells that identified as a neighbor of an analyzed node, but have yet to be fully analyzed
+            var cameFrom = new Dictionary<Point, Point>();          // to back-track from the end to find the optimal path
+            var currentDistance = new Dictionary<Point, int>();     // to store distance from the start  
+            var predictedDistance = new Dictionary<Point, float>(); //to store distance to reach the end
 
             // initialize the start node as having a distance of 0, and an estmated distance 
             // of y-distance + x-distance, which is the optimal path in a square grid that 
             // doesn't allow for diagonal movement
             currentDistance.Add(start, 0);
+            #endregion
+
             predictedDistance.Add(
                 start,
                 0 + +Math.Abs(start.X - end.X) + Math.Abs(start.Y - end.Y)
@@ -101,22 +97,10 @@ namespace TileMap.Algorithm
                     return new List<Point>();
                 }
             }
-
-            // unable to figure out a path, abort.
-            throw new Exception(
-                string.Format(
-                    "unable to find a path between {0},{1} and {2},{3}",
-                    start.X, start.Y,
-                    end.X, end.Y
-                )
-            );
+            return new List<Point>(); //if there is no path betwen end point return empty list
         }
 
-        /// <summary>
-        /// Return a list of accessible nodes neighboring a specified node
-        /// </summary>
-        /// <param name="node">The center node to be analyzed.</param>
-        /// <returns>A list of nodes neighboring the node that are accessible.</returns>
+        // Return a list of accessible nodes neighboring a specified node
         private IEnumerable<Point> GetNeighborNodes(Point node, int[,] Weight)
         {
             var nodes = new List<Point>();
@@ -124,7 +108,7 @@ namespace TileMap.Algorithm
             // up
             if (node.Y - 1 >= 0)
             {
-                if (Weight[node.X, node.Y - 1] == 1 || Weight[node.X, node.Y - 1] == 5)
+                if (Weight[node.X, node.Y - 1] == 1 || Weight[node.X, node.Y - 1] == 5 || Weight[node.X, node.Y - 1] == 6)
                 {
                     nodes.Add(new Point(node.X, node.Y - 1));
                 }
@@ -133,7 +117,7 @@ namespace TileMap.Algorithm
             // right
             if (node.X + 1 <10)
             {
-                if (Weight[node.X + 1, node.Y] == 1 || Weight[node.X + 1, node.Y] == 5)
+                if (Weight[node.X + 1, node.Y] == 1 || Weight[node.X + 1, node.Y] == 5 || Weight[node.X + 1, node.Y] == 6)
             {
                 nodes.Add(new Point(node.X + 1, node.Y));
             }}
@@ -141,7 +125,7 @@ namespace TileMap.Algorithm
             // down
             if (node.Y + 1 < 10)
             {
-                if (Weight[node.X, node.Y + 1] == 1 || Weight[node.X, node.Y + 1] == 5)
+                if (Weight[node.X, node.Y + 1] == 1 || Weight[node.X, node.Y + 1] == 5 || Weight[node.X, node.Y + 1] == 6)
                 {
                     nodes.Add(new Point(node.X, node.Y + 1));
                 }
@@ -150,7 +134,7 @@ namespace TileMap.Algorithm
             // left
             if (node.X - 1 >= 0)
             {
-                if (Weight[node.X - 1, node.Y] == 1 || Weight[node.X - 1, node.Y] == 5)
+                if (Weight[node.X - 1, node.Y] == 1 || Weight[node.X - 1, node.Y] == 5 || Weight[node.X - 1, node.Y] == 6)
                 {
                     nodes.Add(new Point(node.X - 1, node.Y));
                 }
@@ -159,13 +143,8 @@ namespace TileMap.Algorithm
             return nodes;
         }
 
-        /// <summary>
-        /// Process a list of valid paths generated by the Pathfind function and return 
-        /// a coherent path to current.
-        /// </summary>
-        /// <param name="cameFrom">A list of nodes and the origin to that node.</param>
-        /// <param name="current">The destination node being sought out.</param>
-        /// <returns>The shortest path from the start to the destination node.</returns>
+        // Process a list of valid paths generated by the Pathfind function and return 
+        // a coherent path to current.
         private List<Point> ReconstructPath(Dictionary<Point, Point> cameFrom, Point current)
         {
             if (!cameFrom.Keys.Contains(current))
